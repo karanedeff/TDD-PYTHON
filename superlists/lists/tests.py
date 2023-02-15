291688cc546b1f808c99bf37676be94e43d073e0
Preslav Karanedeff <karanedeff@gmail.com>
2023-02-15 12:50:05 +0200

    First unit test and url mapping, dummy view Found a second edition of the book from 2017, still need to translate between book django and modern django.

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -2,10 +2,10 @@ from django.urls import resolve
 from django.test import TestCase
 from .views import home_page
 # Create your tests here.
-class SmokeTest(TestCase):
-
-    def test_bad_maths(self):
-        self.assertEqual(1+1,3)
+# class SmokeTest(TestCase):
+#
+#     def test_bad_maths(self):
+#         self.assertEqual(1+1,3)
 
 
 class HomePageTest(TestCase):
