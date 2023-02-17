794230c3f30d78a005a25aad6417c1c74336c69a
Preslav Karanedeff <karanedeff@gmail.com>
2023-02-17 14:12:22 +0200

    Testing for second item fails

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,7 +1,8 @@
-from selenium import webdriver
 import unittest
 import time
 
+
+from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 
@@ -35,16 +36,22 @@ class NewVisitorTest(unittest.TestCase):
         )
 
         inputbox.send_keys("Buy peacock feathers")
-
         inputbox.send_keys(Keys.ENTER)
+
         time.sleep(1)
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+
+        time.sleep(3)
 
         table = self.browser.find_element(By.ID, "id_list_table")
         rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertTrue(
-            any(row.text == "1: Buy peacock feathers" for row in rows),
-            "New to-do item did not apper in table"
-        )
+        # self.assertTrue(
+        #     any(row.text == "1: Buy peacock feathers" for row in rows),
+        #     f"New to-do item did not apper in table. Contents were:\n{table.text}"
+        # )
+        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
+        self.assertIn("2: Use peacock feathers to make a fly", [row.text for row in rows])
 
         self.fail("Finish the test!")
 
