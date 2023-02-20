31f6c3d514e85ade40fa9faaa784de8841f5a1e2
Preslav Karanedeff <karanedeff@gmail.com>
2023-02-20 10:28:31 +0200

    Model for list Items and associated migration

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -38,16 +38,16 @@ class NewVisitorTest(unittest.TestCase):
             inputbox.get_attribute("placeholder"),
             "Enter a to-do item"
         )
-
         inputbox.send_keys("Buy peacock feathers")
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         self.check_for_row_in_list_table("1: Buy peacock feathers")
 
         # second item
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
-
+        time.sleep(1)
         self.check_for_row_in_list_table("1: Buy peacock feathers")
         self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
         time.sleep(3)
